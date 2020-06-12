b9d15fddde26b7aa93d984b980bf85574fab5bb5
stephaniealley <stephanie.j.alley@gmail.com>
2020-06-12 02:33:13 -0400

    Change X_features_BASC_all to X_features for variable called by train_val

--- Command_Files/train_model.py
+++ Command_Files/train_model.py
@@ -80,7 +80,7 @@ def main():
     X_features = sys.argv[1]
     y_age = sys.argv[2]
     age_class = sys.argv[3]
-    X_train_container, X_val_container, y_train_container, y_val_container = train_val(X_features_BASC_all, y_age, age_class)
+    X_train_container, X_val_container, y_train_container, y_val_container = train_val(X_features, y_age, age_class)
     y_index_all, y_pred_all = model_fit(X_train_container, y_train_container)
     return y_index_all, y_pred_all
 
