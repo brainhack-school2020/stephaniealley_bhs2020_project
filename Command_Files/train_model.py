851953f233d234a59d72b638c200657227dd9b15
stephaniealley <stephanie.j.alley@gmail.com>
2020-06-12 03:30:32 -0400

    Add model training functions to train_model.py

--- Command_Files/train_model.py
+++ Command_Files/train_model.py
@@ -80,10 +80,9 @@ def main():
     X_features = sys.argv[1]
     y_age = sys.argv[2]
     age_class = sys.argv[3]
-    print(X_features)
-    #X_train_container, X_val_container, y_train_container, y_val_container = train_val(X_features, y_age, age_class)
-    #y_index_all, y_pred_all = model_fit(X_train_container, y_train_container)
-    #return y_index_all, y_pred_all
+    X_train_container, X_val_container, y_train_container, y_val_container = train_val(X_features, y_age, age_class)
+    y_index_all, y_pred_all = model_fit(X_train_container, y_train_container)
+    return y_index_all, y_pred_all
 
 if __name__ == "__main__":
     main()
